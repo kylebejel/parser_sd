5c257bdc9d2eca7d933e5ea7bf4484ddd5268d85
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-07-15 13:01:15 -0400

    Update PreprocessFunctions.py

--- code/PreprocessFunctions.py
+++ code/PreprocessFunctions.py
@@ -80,7 +80,7 @@ def preprocess(text):
   for count in range(0,len(tokens)):
     fractions = ["¼", "½", "¾", "⅓", "⅔", "⅕", "⅖", "⅗", "⅘", "⅙", "⅚", "⅛", "⅜", "⅝", "⅞"]
     fracReplace = [".25", ".5", ".75", ".333", ".667", ".2", ".4", ".6", ".8", ".167", ".833", ".125", ".375", ".625", ".875"]
-    for idx, fraction in fractions:
+    for idx, fraction in enumerate(fractions):
       while fraction in word:
         tokens[count] = tokens[count].replace(fraction, fracReplace[idx])
 
