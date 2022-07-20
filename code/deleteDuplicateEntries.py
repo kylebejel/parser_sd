6433149a2414caf587b34ea3ba9e166d27645288
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-07-20 13:41:10 -0400

    Update deleteDuplicateEntries.py

--- code/deleteDuplicateEntries.py
+++ code/deleteDuplicateEntries.py
@@ -1,15 +1,9 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_excel("C_1760_001_FINAL_.xlsx", header=1)
+data = pd.read_excel("C_1760_003_FINAL_.xlsx", header=1)
 
-print(data.shape)
-print(data.to_string())
 for idx in range(0, data.shape[0]-1):
-  print(str(idx) + ' ' + str(data['[EntryID]'][idx+1])[:-1] + ' ' + str(data['[EntryID]'][idx]))
   if str(data['[EntryID]'][idx+1])[:-1] == str(data['[EntryID]'][idx]):#'a' in data['[EntryID]'][idx+1] or 'A' in data['[EntryID]'][idx+1]:
-    print("found one")
     data = data.drop(idx)
 data = data.reset_index(drop=True)
-print(data.shape)
-print(data.to_string())
