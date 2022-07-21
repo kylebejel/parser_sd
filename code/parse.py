b1f4424a34fbca6c5be4e4642794b182452532a4
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-07-21 11:03:10 -0400

    Update parse.py

--- code/parse.py
+++ code/parse.py
@@ -1,4 +1,10 @@
 # code for the function that parses everything
 def parse():
-
+    #Load spreadsheet into pd dataframe
+    #delete rows that need to be deleted from spreadsheet
+    #create new dataframe that will contain parsed info
+    #put all direct transfer information from original sheet into 'parsed' sheet
+    #for rows in entry column:
+        #call preprocess function
+        #call 
     pass
