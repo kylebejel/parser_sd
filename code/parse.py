006b89daa289581c5089f2dcf3040845a406c927
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-07-21 11:12:51 -0400

    Update parse.py

--- code/parse.py
+++ code/parse.py
@@ -6,5 +6,10 @@ def parse():
     #put all direct transfer information from original sheet into 'parsed' sheet
     #for rows in entry column:
         #call preprocess function
-        #call 
+        #for transaction in entry:
+            #call transaction Parsing function
+            #if second or later transaction:
+                #create new row in dataframe at location and copy down information from previous row
+            #add transaction to next line
+    #export to json to load to MongoDB temp database (which will then get loaded to website)
     pass
