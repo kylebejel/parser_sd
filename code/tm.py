17f6888e83caae84bc9e35dc217e2f75be6df135
ckarlson001 <89924386+ckarlson001@users.noreply.github.com>
2022-08-02 18:22:56 -0400

    Update tm.py

--- code/tm.py
+++ code/tm.py
@@ -3,12 +3,20 @@ import re
 def extract_tm(text):
     # can be expanded by appending regex for [int]..[int]..[int]
     # parenthises separate groups
-    tm_pattern = re.compile("\[TM: ([0-9]{4}|[0-9]{5}) ([a-zA-Z]{2}|[a-zA-Z]{3})\] [A-Z] [0-9]")
+    #tm_pattern = re.compile("\[TM: ([0-9]{4}|[0-9]{5}) ([a-zA-Z]{2}|[a-zA-Z]{3})\] [A-Z] ([0-9]*) ")
+    tm_pattern = re.compile("\[TM: ([0-9]{4}|[0-9]{5}) ([a-zA-Z]{2}|[a-zA-Z]{3})\] [A-Z]? ([0-9]*) ([0-9]*)\.\.([0-9]*)\.\.([0-9]*)")
 
     found_tm = tm_pattern.search(text)
 
     # more variables can be made for data values
-    int_id = found_tm.group(1)
-    name_id = found_tm.group(2)
+    TM_id = found_tm.group(1)
+    Mark = found_tm.group(2)
+    note = found_tm.group(3)
+    wt_total = found_tm.group(4)
+    wt_barrel = found_tm.group(5)
+    wt_tobacco = found_tm.group(6)
 
+    tm_parsed = {'TM_id' : TM_id, 'Mark' : Mark, 'note' : note, 'wt_total': wt_total, 'wt_barrel' : wt_barrel, 'wt_tobacco' : wt_tobacco}
+
+    return tm_parsed
     # loop through entries and extract info into variables, and then push to db (need to figure out schema)
