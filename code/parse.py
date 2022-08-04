e26bf03dda8350702dba4a76efc1d4502639eba8
Kyle Bejel <kylebejel@gmail.com>
2022-08-04 01:31:59 -0400

    almost final parse.py

--- code/parse.py
+++ code/parse.py
@@ -92,25 +92,13 @@ def parse(df):
     archmat = df['ArchMat']
     genmat = df['GenMat']
     final = df['Final']
-    error_flag = df['flag']
 
-    ret_list = []
-    ret_size = 0
     counter = 0
-    meta_obj_list = []
-    mentioned_item_obj_list = []
-    acc_holder_obj_list = []
-    date_obj_list = []
-    place_list = []
-    person_list = []
-    note_obj = []
-    tm_list = []
-    pound_shilling_pence_list = []
-    money_list = []
 
-    for transaction in parsed_transactions:
-        temp_row = [38] * None
+    for transaction_counter in range(0,len(parsed_transactions)):
+    # for transaction in parsed_transactions:
         # assignments
+        # transaction = parsed_transactions[transaction_counter]
 
         # meta obj
         meta_obj = {}
@@ -171,54 +159,33 @@ def parse(df):
         money_obj['sterling'] = s_psp
         money_obj['currency'] = c_psp
         
-        # person obj
-
-        # place obj
-
-
-        # temp_row[0]=transcriber_time[counter]
-        # temp_row[1]=file_name[counter]
-        # temp_row[2]=reel[counter]
-        # temp_row[3]=owner[counter]
-        # temp_row[4]=store[counter]
-        # temp_row[5]=folio_year[counter]
-        # temp_row[6]=folio_page[counter]
-        # temp_row[7]=entry_id[counter]
-        # temp_row[8]=prefix[counter]
-        # temp_row[9]=account_firstname[counter]
-        # temp_row[10]=account_lastname[counter]
-        # temp_row[11]=suffix[counter]
-        # temp_row[12]=profession[counter]
-        # temp_row[13]=location[counter]
-        # temp_row[14]=reference[counter]
-        # temp_row[15]=drcr[counter]
-        # # TEMPORARY YEAR CHANGE THIS
-        # # temp_year = df['Temp Year']
-        # temp_row[16] = year[counter]
-        # temp_row[17] = month[counter]
-        # temp_row[18] = day[counter]
-        # temp_row[22] = folio_reference[counter]
-        # temp_row[25] = quantity[counter]
-        # temp_row[26] = commodity[counter]
-        # temp_row[27] = SL[counter]
-        # temp_row[28] = SS[counter]
-        # temp_row[29] = SD[counter]
-        # temp_row[30] = colony[counter]
-        # temp_row[31] = CL[counter]
-        # temp_row[32] = CS[counter]
-        # temp_row[33] = CD[counter]
-        # temp_row[34] = archmat[counter]
-        # temp_row[35] = genmat[counter]
-        # temp_row[36] = final[counter]
-        # # FIX EXTRA NOTES
-        # temp_row[37] = error_flag[counter]
-
         if counter in repeated_idxs.keys:
             for x in range(0,repeated_idxs[counter]):
                 # people, places, ledger, entry type, flag?
                 entry_obj = {}
+                item_entry_obj = {}
+                item_or_service_obj = {}
+
+                transaction = parsed_transactions[transaction_counter]
+
+                item_or_service_obj['quantity'] = transaction['quantity']
+                item_or_service_obj['qualifier'] = transaction['qualifier']
+                item_or_service_obj['variants'] = transaction['variants']
+                item_or_service_obj['item'] = transaction['item']
+                item_or_service_obj['category'] = transaction['category']
+                item_or_service_obj['subcategory'] = None
+                item_or_service_obj['unitCost'] = transaction['unitCost']
+                item_or_service_obj['itemCost'] = transaction['totalCost']
+
+                item_entry_obj['perOrder'] = None
+                item_entry_obj['percentage'] = None
+                item_entry_obj['itemOrServices'] = []
+                item_entry_obj['itemOrServices'].append(item_or_service_obj)
+                item_entry_obj['itemsMentioned'] = None
+
                 people_obj_list = []
                 places_obj_list = []
+
                 for person in transaction['mentionedPpl']:
                     person_obj = {}
                     person_obj['name'] = person
@@ -227,52 +194,101 @@ def parse(df):
                 for place in transaction['mentionedPlaces']:
                     place_obj = {}
                     place_obj['name'] = place
-                    place_list.append(place_obj)
+                    places_obj_list.append(place_obj)
                 
-                temp_transaction = transaction
-                temp_transaction[] = temp_transaction
-
                 entry_obj['accountHolder'] = acc_holder_obj
                 entry_obj['meta'] = meta_obj
                 entry_obj['dateInfo'] = date_obj
                 entry_obj['folioRefs'] = folio_reference[counter]
                 entry_obj['ledgerRefs'] = None
-                entry_obj['itemEntries?'] =
-                entry_obj['tobaccoEntry?'] =
+                entry_obj['itemEntries?'] = []
+                entry_obj['itemEntries?'].append(item_entry_obj)
+                entry_obj['tobaccoEntry?'] = None # CHANGE THIS 
                 entry_obj['regularEntry?'] = None
                 entry_obj['people'] = people_obj_list
                 entry_obj['places'] = places_obj_list
                 entry_obj['entry'] = entry[counter]
                 entry_obj['money'] = money_obj
+                entry_obj['errorReview'] = transaction['errorReview']
 
                 # append
-                ret_list.append(temp_row)
+                # ret_list.append(temp_row)
+                entry_obj_list.append(entry_obj)
+                transaction_counter+=1
 
         else:
-            temp_row[19] = transaction['entry']
-            temp_row[20] = transaction['people']
-            temp_row[21] = transaction['places']
-            temp_row[23] = transaction['entry_type']
-            temp_row[24] = transaction['ledger']
-            # append
-            ret_list.append(temp_row)
+            entry_obj = {}
+            item_entry_obj = {}
+            item_or_service_obj = {}
+
+            transaction = parsed_transactions[transaction_counter]
+
+            item_or_service_obj['quantity'] = transaction['quantity']
+            item_or_service_obj['qualifier'] = transaction['qualifier']
+            item_or_service_obj['variants'] = transaction['variants']
+            item_or_service_obj['item'] = transaction['item']
+            item_or_service_obj['category'] = transaction['category']
+            item_or_service_obj['subcategory'] = None
+            item_or_service_obj['unitCost'] = transaction['unitCost']
+            item_or_service_obj['itemCost'] = transaction['totalCost']
+
+            item_entry_obj['perOrder'] = None
+            item_entry_obj['percentage'] = None
+            item_entry_obj['itemOrServices'] = []
+            item_entry_obj['itemOrServices'].append(item_or_service_obj)
+            item_entry_obj['itemsMentioned'] = None
+
+            people_obj_list = []
+            places_obj_list = []
+
+            for person in transaction['mentionedPpl']:
+                person_obj = {}
+                person_obj['name'] = person
+                people_obj_list.append(person_obj)
+
+            for place in transaction['mentionedPlaces']:
+                place_obj = {}
+                place_obj['name'] = place
+                places_obj_list.append(place_obj)
+                
+                
+
+            entry_obj['accountHolder'] = acc_holder_obj
+            entry_obj['meta'] = meta_obj
+            entry_obj['dateInfo'] = date_obj
+            entry_obj['folioRefs'] = folio_reference[counter]
+            entry_obj['ledgerRefs'] = None
+            entry_obj['itemEntries?'] = []
+            entry_obj['itemEntries?'].append(item_entry_obj)
+            entry_obj['tobaccoEntry?'] = None # CHANGE THIS 
+            entry_obj['regularEntry?'] = None
+            entry_obj['people'] = people_obj_list
+            entry_obj['places'] = places_obj_list
+            entry_obj['entry'] = entry[counter]
+            entry_obj['money'] = money_obj
+            entry_obj['errorReview'] = transaction['errorReview']
+
+                # append
+                # ret_list.append(temp_row)
+            entry_obj_list.append(entry_obj)
+            transaction_counter+=1
+
         # increment
         counter+=1
         
-    final_df = pd.DataFrame(ret_list, columns = ['Column_A','Column_B','Column_C'])
+    return entry_obj_list
 
 
     # --------------------------------------------------------------------
 
 def main():
     cwd = Path.cwd()
-    above = cwd.parent / 'util/data/original'
+    fp = cwd / 'C_1760_002_FINAL_.xlsx'
     # print(f'dir is : {above}')
 
-    for file in above.iterdir():
-        df = pd.read_excel(file)
-        new_df = parse(df)
-        # print(new_df.head)
+    df = pd.read_excel(fp)
+    entry_objs = parse(df)
+    print(entry_objs)
 
 if __name__ == '__main__':
     main()
